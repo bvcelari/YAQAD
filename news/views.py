10def25fc68ef5c57da49963d7997dd7e19c969e
bvcelari <bvcelari@gmail.com>
2013-11-17 18:19:29 +0100

    Autocomplete in Question:pending save logic in view

--- news/views.py
+++ news/views.py
@@ -125,14 +125,13 @@ def ajax_add_question(request):
 				writer.questions.add(addquestion.id)
 				#add topics
 				topiclist = request.POST['name']
-				for t in topiclist:
-					print(t.value)       
+				for t in topiclist.split(', ')[:-1]:
+					print("  -  "+t)       
 					#if q in t.name:
 				        #     value = '%s ' % (t.name,)
 				        #     t_dict = {'id': t.id, 'label': value, 'value': value}
 				        #     topics_list.append(t_dict)
 
-				print(topiclist)
                                 state = 2
 				result = simplejson.dumps({  
 					'message': "Success message", 
